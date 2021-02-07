033c6a5fc11f25d09314a0cf3725efa2ae808667
Chisato Murakami <kiwipom1031@gmail.com>
2021-02-07 12:42:55 -0800

    made get_five_days_result method. It displays 5days forecast in show page.

--- app/controllers/screens_controller.rb
+++ app/controllers/screens_controller.rb
@@ -6,9 +6,21 @@ class ScreensController < ApplicationController
   def show
     searched_city = params.require(:city)
     response = HTTParty.get("http://api.openweathermap.org/data/2.5/forecast?q=#{searched_city}&appid=" + ENV["WEATHER_API_KEY"])
-    @city = response.parsed_response["city"]["name"]
-    @weather_now = response.parsed_response["list"][0]["weather"][0]["main"]
-    @weather_description = response.parsed_response["list"][0]["weather"][0]["description"]
+    parsed_response = response.parsed_response
+    
+    @city = parsed_response["city"]["name"]
+    all_results = parsed_response["list"]
+    @weather_now = all_results[0]["weather"][0]["main"]
+    @weather_description = all_results[0]["weather"][0]["description"]
+    @five_days_result = get_five_days_result(all_results)
+  end
+
+  def get_five_days_result(response_list)
+    five_days_result = []
+    (0..4).to_a.each do |i|
+      five_days_result.push(response_list[i * 8])
+    end
+    five_days_result
   end
 end
 
