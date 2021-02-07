c499aa7a3730fa028e9952127f1f43cc8212ed38
Chisato Murakami <kiwipom1031@gmail.com>
2021-02-07 11:39:22 -0800

    display weather now and description in show page

--- app/controllers/screens_controller.rb
+++ app/controllers/screens_controller.rb
@@ -4,8 +4,11 @@ class ScreensController < ApplicationController
   end
   
   def show
-    @searched_city = params.require(:city)
-    response = HTTParty.get("http://api.openweathermap.org/data/2.5/forecast?q=#{@searched_city}&appid=" + ENV["WEATHER_API_KEY"])
+    searched_city = params.require(:city)
+    response = HTTParty.get("http://api.openweathermap.org/data/2.5/forecast?q=#{searched_city}&appid=" + ENV["WEATHER_API_KEY"])
+    @city = response.parsed_response["city"]["name"]
+    @weather_now = response.parsed_response["list"][0]["weather"][0]["main"]
+    @weather_description = response.parsed_response["list"][0]["weather"][0]["description"]
   end
 end
 
