ce4232ffd7234539f16062965412b02c779863d0
Chisato Murakami <kiwipom1031@gmail.com>
2021-02-07 11:21:58 -0800

    add dotenv gem

--- Gemfile
+++ Gemfile
@@ -46,3 +46,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "httparty", "~> 0.18.1"
+
+gem "dotenv-rails", "~> 2.7"
--- app/controllers/screens_controller.rb
+++ app/controllers/screens_controller.rb
@@ -1,9 +1,11 @@
 class ScreensController < ApplicationController
   def index
-
+    
   end
   
   def show
-    
+    @searched_city = params.require(:city)
+    response = HTTParty.get("http://api.openweathermap.org/data/2.5/forecast?q=#{@searched_city}&appid=" + ENV["WEATHER_API_KEY"])
   end
 end
+
